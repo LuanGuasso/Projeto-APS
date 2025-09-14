4c0ce911de09d3bae5cfc8c9eced60cf87ea57e5
Luan Guasso <luanguasso04@gmail.com>
2025-09-14 16:52:16 -0300

    indentação e adicionada imagem de fundo da unisc nas telas

--- Back-End/db.js
+++ Back-End/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",     
-  password: "",     
-  database: "sistema_cadastro"
+  user: "root",
+  password: "",
+  database: "sistema_cadastro",
 });
 
 db.connect((err) => {
--- Back-End/server.js
+++ Back-End/server.js
@@ -11,35 +11,67 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/cadastro", async (req, res) => {
   const {
-    tipo, nome, data_nascimento, contato, cpf, rg,
-    cidade, endereco, estado_civil, sexo, nome_pai, nome_mae, senha
+    tipo,
+    nome,
+    data_nascimento,
+    contato,
+    cpf,
+    rg,
+    cidade,
+    endereco,
+    estado_civil,
+    sexo,
+    nome_pai,
+    nome_mae,
+    senha,
   } = req.body;
 
   try {
-    db.query("SELECT * FROM usuarios WHERE nome = ?", [nome], async (err, results) => {
-      if (err) return res.status(500).json({ error: "Erro no servidor" });
+    db.query(
+      "SELECT * FROM usuarios WHERE nome = ?",
+      [nome],
+      async (err, results) => {
+        if (err) return res.status(500).json({ error: "Erro no servidor" });
 
-      if (results.length > 0) {
-        return res.status(400).json({ error: "Usuário já cadastrado!" });
-      }
+        if (results.length > 0) {
+          return res.status(400).json({ error: "Usuário já cadastrado!" });
+        }
 
-      const hashSenha = await bcrypt.hash(senha, 10);
+        const hashSenha = await bcrypt.hash(senha, 10);
 
-      const sql = `INSERT INTO usuarios 
+        const sql = `INSERT INTO usuarios 
         (tipo, nome, data_nascimento, contato, cpf, rg, cidade, endereco, estado_civil, sexo, nome_pai, nome_mae, senha) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-      db.query(sql, [
-        tipo, nome, data_nascimento, contato, cpf, rg, cidade,
-        endereco, estado_civil, sexo, nome_pai, nome_mae, hashSenha
-      ], (err2) => {
-        if (err2) {
-          console.error(err2);
-          return res.status(500).json({ error: "Erro ao cadastrar usuário" });
-        }
-        res.json({ message: "Usuário cadastrado com sucesso!" });
-      });
-    });
+        db.query(
+          sql,
+          [
+            tipo,
+            nome,
+            data_nascimento,
+            contato,
+            cpf,
+            rg,
+            cidade,
+            endereco,
+            estado_civil,
+            sexo,
+            nome_pai,
+            nome_mae,
+            hashSenha,
+          ],
+          (err2) => {
+            if (err2) {
+              console.error(err2);
+              return res
+                .status(500)
+                .json({ error: "Erro ao cadastrar usuário" });
+            }
+            res.json({ message: "Usuário cadastrado com sucesso!" });
+          }
+        );
+      }
+    );
   } catch (error) {
     res.status(500).json({ error: "Erro interno do servidor" });
   }
@@ -52,25 +84,30 @@ app.post("/login", (req, res) => {
     return res.status(400).json({ error: "Informe nome e senha!" });
   }
 
-  db.query("SELECT * FROM usuarios WHERE nome = ?", [nome], async (err, results) => {
-    if (err) return res.status(500).json({ error: "Erro no servidor" });
+  db.query(
+    "SELECT * FROM usuarios WHERE nome = ?",
+    [nome],
+    async (err, results) => {
+      if (err) return res.status(500).json({ error: "Erro no servidor" });
 
-    if (results.length === 0) {
-      return res.status(401).json({ error: "Usuário não encontrado. Faça o cadastro primeiro!" });
+      if (results.length === 0) {
+        return res
+          .status(401)
+          .json({ error: "Usuário não encontrado. Faça o cadastro primeiro!" });
+      }
+
+      const usuario = results[0];
+      const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
+
+      if (!senhaCorreta) {
+        return res.status(401).json({ error: "Senha incorreta!" });
+      }
+
+      res.json({ message: "Login realizado com sucesso", usuario });
     }
-
-    const usuario = results[0];
-    const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
-
-    if (!senhaCorreta) {
-      return res.status(401).json({ error: "Senha incorreta!" });
-    }
-
-    res.json({ message: "Login realizado com sucesso", usuario });
-  });
+  );
 });
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
-
